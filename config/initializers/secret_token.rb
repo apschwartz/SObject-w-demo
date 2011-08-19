f07101724e283b04863046a913603d0b38b2d501
Andy Schwartz <andy-git@schegg.org>
2011-08-19 11:17:08 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sfplay1::Application.config.secret_token = '8a92e57f91bae6938d1f5815e15e8b07c83e335471e0820cc0cf82e734ffa799fedfe398b9aad225cc5eb440b75ce58db78411f771ac4365a86671f774992683'
