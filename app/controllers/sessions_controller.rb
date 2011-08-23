831776d20c954cbf3dd7427f9524d45507c882b6
Andy Schwartz <andy-git@schegg.org>
2011-08-22 21:05:08 -0700

    more changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,6 @@ class SessionsController < ApplicationController
     ENV['sfdc_api_version'] = '21.0'
     ENV['sfdc_token'] = request.env['omniauth.auth']['credentials']['token']
     ENV['sfdc_instance_url'] = request.env['omniauth.auth']['instance_url']
-    # render :text => request.env['omniauth.auth'].inspect
-    # render :text => Accounts.get_first_hundred.inspect
     redirect_to :controller => 'sessions', :action => 'bom_rev_with_lineitems'
   end
   def fail
